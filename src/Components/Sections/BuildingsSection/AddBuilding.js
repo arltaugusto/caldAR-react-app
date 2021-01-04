ae004d0ff8aebf69b045db4342b206ac7c865eb1
Ignacio Aristo <ignacioaristo@gmail.com>
2021-01-04 19:32:18 -0300

    form first commit

--- src/Components/Sections/BuildingsSection/AddBuilding.js
+++ src/Components/Sections/BuildingsSection/AddBuilding.js
@@ -1,14 +1,67 @@
 import React from 'react';
+import { Form, Field } from 'react-final-form';
 
 const AddBuilding = (props) => (
-  <form onSubmit={props.onSubmit}>
-    <input type='text' name='address' placeholder='Address' value={props.buildingForm.address} onChange={props.onChange} />
-    <input type='text' name='name' placeholder='Name' value={props.buildingForm.name} onChange={props.onChange}/>
-    <input type='number' name='phone' placeholder='Phone' value={props.buildingForm.phone} onChange={props.onChange}/>
-    <input type='string' name='idCustomer' placeholder='IdCustomer' value={props.buildingForm.idCustomer} onChange={props.onChange}/>
-    <input type='string' name='boilers' placeholder='Boilers' value={props.buildingForm.boilers} onChange={props.onChange}/>
-    <input type='submit' value='Send' />
-  </form>
+  <Form
+    onSubmit={props.onSubmit}
+    render={(
+      {
+        handleSubmit, form,
+      },
+    ) => (
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Address:</label>
+          <Field
+            type='text'
+            name='address'
+            placeholder='Address'
+            value={props.buildingForm.address}
+            component='input'
+          />
+        </div>
+        <div>
+          <label>Name:</label>
+          <Field
+            type='text'
+            name='name'
+            placeholder='Name'
+            value={props.buildingForm.name}
+            component='input'
+          />
+        </div>
+        <div>
+          <label>Phone</label>
+          <Field type='number'
+          name='phone'
+          placeholder='Phone'
+          value={props.buildingForm.phone}
+          component='input'
+          />
+        </div>
+        <div>
+          <label>Id Customer</label>
+          <Field type='string'
+          name='idCustomer'
+          placeholder='IdCustomer'
+          value={props.buildingForm.idCustomer}
+          component='input'
+          />
+        </div>
+        <div>
+        <label>Boilers</label>
+          <Field type='string'
+          name='boilers'
+          placeholder='Boilers'
+          value={props.buildingForm.boilers}
+          component='input'
+          />
+        </div>
+        <div>
+        <input type='submit' value='Send' />
+        </div>
+      </form>
+    )}
+  />
 );
-
 export default AddBuilding;
